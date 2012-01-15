49d163c05084bb83cbe114bbe1a5c9f94ac37bd1
jonnosan <jonnosan@93682198-c243-4bdb-bd91-e943c89aac3b>
2012-01-15 04:59:59 +0000

    start of C bindings
    git-svn-id: http://svn.code.sf.net/p/netboot65/code@328 93682198-c243-4bdb-bd91-e943c89aac3b

---
+++ client/libnet/libnet.h
@@ -0,0 +1,47 @@
+
+#ifndef _LIBNET_H
+#define _LIBNET_H
+
+
+/* IP configuration structure */
+typedef unsigned char u8;
+typedef unsigned char libnet_err_t;
+
+typedef struct ip_config {
+	u8 ip_addr[4];
+	u8 netmask[4];
+	u8 gateway_addr[4];
+	u8 dns_server_addr[4];		
+} IP_CONFIG;
+
+
+#define LIBNET_USE_DHCP 0x0000
+
+extern libnet_err_t __fastcall__ libnet_init (IP_CONFIG* config_p);
+extern void __fastcall__ libnet_get_config (IP_CONFIG* config_p);
+extern char libnet_MAC[6];
+
+
+
+
+#define LIBNET_OK				                    0x00
+#define LIBNET_ERROR_PORT_IN_USE                    0x80
+#define LIBNET_ERROR_TIMEOUT_ON_RECEIVE             0x81
+#define LIBNET_ERROR_TRANSMIT_FAILED                0x82
+#define LIBNET_ERROR_TRANSMISSION_REJECTED_BY_PEER  0x83
+#define LIBNET_ERROR_INPUT_TOO_LARGE                0x84
+#define LIBNET_ERROR_DEVICE_FAILURE                 0x85
+#define LIBNET_ERROR_ABORTED_BY_USER                0x86
+#define LIBNET_ERROR_LISTENER_NOT_AVAILABLE         0x87
+#define LIBNET_ERROR_NO_SUCH_LISTENER               0x88
+#define LIBNET_ERROR_CONNECTION_RESET_BY_PEER       0x89
+#define LIBNET_ERROR_CONNECTION_CLOSED              0x8A
+#define LIBNET_ERROR_TOO_MANY_ERRORS                0x8B
+#define LIBNET_ERROR_FILE_ACCESS_FAILURE            0x90
+#define LIBNET_ERROR_MALFORMED_URL                  0xA0
+#define LIBNET_ERROR_DNS_LOOKUP_FAILED              0xA1
+#define LIBNET_ERROR_OPTION_NOT_SUPPORTED           0xFE
+#define LIBNET_ERROR_FUNCTION_NOT_SUPPORTED         0xFF
+
+
+#endif
---
+++ client/libnet/libnet.s
@@ -0,0 +1,95 @@
+.export _libnet_init
+.export _libnet_get_config
+.export	_libnet_MAC
+
+.import cfg_init
+.import cfg_ip
+.import copymem
+.import ip65_init
+.import dhcp_init
+.import ip65_error
+.import cfg_mac_default
+.import cfg_mac
+.import cfg_size
+
+_libnet_MAC=cfg_mac_default
+
+; load A/X macro
+.macro ldax arg
+.if (.match (.left (1, arg), #))	; immediate mode
+	lda #<(.right (.tcount (arg)-1, arg))
+	ldx #>(.right (.tcount (arg)-1, arg))
+.else					; assume absolute or zero page
+	lda arg
+	ldx 1+(arg)
+.endif
+.endmacro
+
+; store A/X macro
+.macro stax arg
+	sta arg
+	stx 1+(arg)
+.endmacro	
+
+
+NO_ERROR=$00
+
+
+.importzp copy_src
+.importzp copy_dest
+.zeropage
+
+.code
+_libnet_init:
+	sta		copy_src
+	stx		copy_src+1
+	beq		@dhcp_request
+	ldax	#cfg_ip
+	stax	copy_dest
+  	ldax  	#$10			;4 items of config data	
+  	jsr 	copymem	
+	jsr 	ip65_init
+@check_error:	
+	bcc 	@ok
+	lda		ip65_error
+	rts
+@ok:
+	lda		#NO_ERROR
+	rts
+	
+	
+@dhcp_request:
+	jsr 	ip65_init
+	jsr		dhcp_init
+	jmp		@check_error
+
+_libnet_get_config:
+
+	stax	copy_dest
+	ldax	#cfg_ip
+	stax	copy_src
+  	ldax  	#$10			;4 items of config data
+  	
+  	jmp copymem
+
+.bss
+tmp_ax: .res 2
+
+;-- LICENSE FOR libnet.s --
+; The contents of this file are subject to the Mozilla Public License
+; Version 1.1 (the "License"); you may not use this file except in
+; compliance with the License. You may obtain a copy of the License at
+; http://www.mozilla.org/MPL/
+; 
+; Software distributed under the License is distributed on an "AS IS"
+; basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+; License for the specific language governing rights and limitations
+; under the License.
+; 
+; The Original Code is libnet.
+; 
+; The Initial Developer of the Original Code is Jonno Downes,
+; jonno@jamtronix.com.
+; Portions created by the Initial Developer are Copyright (C) 2012
+; Jonno DOwnes. All Rights Reserved.  
+; -- LICENSE END --
