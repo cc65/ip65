879651bb3e479c8549896bcabc026648ed74f562
Oliver Schmidt <ol.sc@web.de>
2017-11-09 21:48:08 +0100

    Added C interface to SNTP function.

---
+++ ip65/sntp_c.s
@@ -0,0 +1,26 @@
+.include "../inc/common.i"
+
+.export _sntp_get_time
+
+.import sntp_get_time
+.import sntp_ip
+.import sntp_utc_timestamp
+
+.importzp sreg
+
+_sntp_get_time:
+  stax sntp_ip
+  ldax sreg
+  stax sntp_ip+2
+  jsr sntp_get_time
+  bcs error
+  ldax sntp_utc_timestamp+2
+  stax sreg
+  ldax sntp_utc_timestamp
+  rts
+
+error:
+  ldx #$00
+  txa
+  stax sreg
+  rts
