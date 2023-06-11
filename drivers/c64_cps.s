821a63c0c219c0365bdfe58ac6699c9d9a2423e3
Oliver Schmidt <ol.sc@web.de>
2023-06-11 12:23:45 +0200

    Adjusted to cc65/cc65#1844

--- drivers/c64_cps.s
+++ drivers/c64_cps.s
@@ -2,11 +2,11 @@
 ; CC65's C64 runtime library doesn't provide this function
 ; this file provides a version in order that clk_timer.s works without 'ifdefs'
 
-.export __clocks_per_sec
+.export ___clocks_per_sec
 
 .code
 
-__clocks_per_sec:
+___clocks_per_sec:
   lda #60
   rts
 
